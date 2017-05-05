ef9b7453c52123de3ce0bec231615782bb757e83
Timo Kersten <kersten@in.tum.de>
2017-05-05 17:24:53 +0200

    Added ldraw export

--- main.cpp
+++ main.cpp
@@ -67,12 +67,12 @@ public:
       availableColors.push_back(create);
   }
 
-  ColorSpec* getSpecFromPalette(ColorSpec::Color c){
+  ColorSpec* getSpecFromPalette(ColorSpec::Color c, int partId){
     double minDist = std::numeric_limits<double>::max();
-    auto* minSpec = &availableColors[0];
+    ColorSpec* minSpec = nullptr;
     for(auto& spec : availableColors){
       double dist = pow(c.r-(double)spec.color.r,2) + pow(c.g-(double)spec.color.g,2) + pow(c.b-(double)spec.color.b,2);
-      if(dist < minDist){
+      if(spec.availability.indexed[partId] == '+' && dist < minDist){
         minDist=dist;
         minSpec=&spec;
       }
@@ -82,7 +82,29 @@ public:
 
 };
 
-void groupByMask(Mat image, Mat mask, uint64_t groups, Palette& palette){
+struct MaskConfig{
+  Mat mask;
+  std::vector<uint8_t> groupIdToPart;
+};
+
+MaskConfig circleMask(int32_t circlesLongSide, int32_t imageLongSide){
+  int32_t sideLength = imageLongSide / circlesLongSide;
+  MaskConfig mc;
+  Mat& mask = mc.mask;
+  mask = Mat::zeros(sideLength,sideLength, CV_8UC1);
+  rectangle(mask, {Point(0,0),                      Point(sideLength/2,sideLength/2)}, 0, -1);
+  rectangle(mask, {Point(sideLength/2,0),           Point(sideLength,sideLength/2)}, 1, -1);
+  rectangle(mask, {Point(0,sideLength/2),           Point(sideLength/2,sideLength)}, 2, -1);
+  rectangle(mask, {Point(sideLength/2,sideLength/2),Point(sideLength,sideLength)}, 3, -1);
+  circle(mask, {sideLength/2,sideLength/2}, sideLength/2, 4, -1);
+  circle(mask, {sideLength/2,sideLength/2}, sideLength/5, 5, -1);
+  mc.groupIdToPart = {1,1,1,1,3,2};
+  return mc;
+}
+
+template<typename C>
+void groupByMask(Mat image, MaskConfig mc, uint64_t groups, Palette& palette, C onTileDone){
+  Mat& mask = mc.mask;
   // accept only char type matrices
   CV_Assert(image.depth() == CV_8U);
   // accept only images with 3 channels
@@ -91,14 +113,14 @@ void groupByMask(Mat image, Mat mask, uint64_t groups, Palette& palette){
   uint64_t tileWidth = mask.cols;
 
   // iterate over tiles of size mask
-  for (int r = 0; r < image.rows; r += tileHeight)
-    for (int c = 0; c < image.cols; c += tileWidth){
+  for (int r = 0, rc=0; r < image.rows; r += tileHeight, rc++)
+    for (int c = 0, cc=0; c < image.cols; c += tileWidth, cc++){
       Mat tile = image(Range(r, std::min(r + tileHeight, static_cast<uint64_t>(image.rows))),
                        Range(c, std::min(c + tileWidth, static_cast<uint64_t>(image.cols))));//no data copying here
       // create group averages for the tile
       uint64_t count[groups*3];
       uint64_t sum[groups*3];
-      char avg[groups*3];
+      uint8_t avg[groups*3];
       for(int i = 0; i < 3*groups; i++){
         count[i] = 0;
         sum[i] = 0;
@@ -114,12 +136,14 @@ void groupByMask(Mat image, Mat mask, uint64_t groups, Palette& palette){
         }
       }
       //create group values
-      for(int i = 0; i < 3*groups; i+=3){
+      for(int group = 0; group < groups; group++){
+        const int i = group*3;
         // find closest color
         auto spec = palette.getSpecFromPalette({
             static_cast<uint16_t>(sum[i+2]/std::max(count[i+2], 1ull)),
             static_cast<uint16_t>(sum[i+1]/std::max(count[i+1], 1ull)),
-            static_cast<uint16_t>(sum[i]/std::max(count[i], 1ull))});
+            static_cast<uint16_t>(sum[i]/std::max(count[i], 1ull))},
+            mc.groupIdToPart[group]);
         avg[i] = spec->color.b;
         avg[i+1] = spec->color.g;
         avg[i+2] = spec->color.r;
@@ -133,20 +157,10 @@ void groupByMask(Mat image, Mat mask, uint64_t groups, Palette& palette){
           (*it)[i] = avg[group*3+i];
         }
       }
+      onTileDone(rc,cc,avg);
     }
 }
 
-Mat circleMask(int32_t circlesLongSide, int32_t imageLongSide){
-  int32_t sideLength = imageLongSide / circlesLongSide;
-  Mat mask = Mat::zeros(sideLength,sideLength, CV_8UC1);
-  rectangle(mask, {Point(0,0),Point(sideLength/2,sideLength/2)}, 0, -1);
-  rectangle(mask, {Point(sideLength/2,0),Point(sideLength,sideLength/2)}, 1, -1);
-  rectangle(mask, {Point(0,sideLength/2),Point(sideLength/2,sideLength)}, 2, -1);
-  rectangle(mask, {Point(sideLength/2,sideLength/2),Point(sideLength,sideLength)}, 3, -1);
-  circle(mask, {sideLength/2,sideLength/2}, sideLength/2, 4, -1);
-  circle(mask, {sideLength/2,sideLength/2}, sideLength/5, 5, -1);
-  return mask;
-}
 
 
 struct PaintState{
@@ -154,6 +168,9 @@ struct PaintState{
   Mat image;
   int sigma = 200, threshold = 500, amount = 100;
   Palette palette;
+  bool showMosaic = true;
+  bool writeLDrawFile = false;
+  string ldrawFileName;
 }state;
 
 void sharpen(Mat& img){
@@ -167,15 +184,40 @@ void sharpen(Mat& img){
   img=sharpened;
 }
 
+void writeLDrawFile(int x, int y, uint8_t* avgs, ofstream& ldFile)
+/// Write mosaic in LDraw format into ldFile 
+{
+  ldFile << "1 0x2" << std::hex << static_cast<int>(avgs[3*5+2]) << static_cast<int>(avgs[3*5+1]) << static_cast<int>(avgs[3*5])
+         << std::dec << " " << (y * 20 * 2) << " -16 " << (-x * 20 * 2 + 10) <<   " 1 0 0 0 1 0 -0 0 1 6141.dat\n";
+  ldFile << "1 0x2" << std::hex << static_cast<int>(avgs[3*4+2]) << static_cast<int>(avgs[3*4+1]) << static_cast<int>(avgs[3*4])
+         << std::dec << " " << (y * 20 * 2) << " -8 " << (-x * 20 * 2 + 10) <<     " 1 0 0 0 1 0 -0 0 1 18674.dat\n";
+  ldFile << "1 0x2" << std::hex << static_cast<int>(avgs[3*0+2]) << static_cast<int>(avgs[3*0+1]) << static_cast<int>(avgs[3*0])
+         << std::dec << " " << (y * 20 * 2 - 10) << " 0 " << (-x * 20 * 2) <<      " 1 0 0 0 1 0 -0 0 1 3024.dat\n";
+  ldFile << "1 0x2" << std::hex << static_cast<int>(avgs[3*1+2]) << static_cast<int>(avgs[3*1+1]) << static_cast<int>(avgs[3*1])
+         << std::dec << " " << (y * 20 * 2 - 10) << " 0 " << (-x * 20 * 2 + 20) << " 1 0 0 0 1 0 -0 0 1 3024.dat\n";
+  ldFile << "1 0x2" << std::hex << static_cast<int>(avgs[3*2+2]) << static_cast<int>(avgs[3*2+1]) << static_cast<int>(avgs[3*2])
+         << std::dec << " " << (y * 20 * 2 + 10) << " 0 " << (-x * 20 * 2) <<      " 1 0 0 0 1 0 -0 0 1 3024.dat\n";
+  ldFile << "1 0x2" << std::hex << static_cast<int>(avgs[3*3+2]) << static_cast<int>(avgs[3*3+1]) << static_cast<int>(avgs[3*3])
+         << std::dec << " " << (y * 20 * 2 + 10) << " 0 " << (-x * 20 * 2 + 20) << " 1 0 0 0 1 0 -0 0 1 3024.dat\n";
+}
+
 void repaint(){
   state.image = state.original.clone();
   Mat& image = state.image;
-  auto mask = circleMask(48, max(image.rows, image.cols));
+  auto mc = circleMask(48, max(image.rows, image.cols));
   sharpen(image);
-  groupByMask(image, mask, 6, state.palette);
+  ofstream ldFile;
+  if (state.ldrawFileName.size() && state.writeLDrawFile)
+    ldFile = ofstream(state.ldrawFileName);
+  if(state.showMosaic)
+    groupByMask(image, mc, 6, state.palette, [&ldFile](int x, int y, uint8_t* avgs){
+        if (state.ldrawFileName.size() && state.writeLDrawFile)
+          writeLDrawFile(x,y,avgs, ldFile);
+      });
   Mat disp;
-  resize(image, disp, Size(1024,768));
-  imshow("Display Image", disp);
+  //resize(image, disp, Size(1024,768));
+  //imshow("Display Image", disp);
+  imshow("Display Image", image);
 }
 
 void on_trackbar( int, void* ){
@@ -184,7 +226,7 @@ void on_trackbar( int, void* ){
 
 int main(int argc, char* argv[]){
   if (argc < 3){
-    std::cout << "usage: " << argv[0] << " <paletteFile> <inputImage> [<outputImage>]\n";
+    std::cout << "usage: " << argv[0] << " <paletteFile> <inputImage> [<outputImage>] [<outputLDrawFile>]\n";
     return -1;
   }
   state.palette = Palette(argv[1]);
@@ -194,12 +236,25 @@ int main(int argc, char* argv[]){
     return -1;
   }
 
-  namedWindow("Display Image", WINDOW_AUTOSIZE);
-  createTrackbar( "Amount", "Display Image", &state.amount ,1000, on_trackbar);
-  createTrackbar( "Sigma", "Display Image", &state.sigma ,1000, on_trackbar);
-  createTrackbar( "BG Threshold", "Display Image", &state.threshold ,1000, on_trackbar);
+  if(argc>=5)
+    state.ldrawFileName = argv[4];
+
+  namedWindow("Display Image",CV_WINDOW_AUTOSIZE | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED);
+  namedWindow("Toolbar",CV_WINDOW_NORMAL | CV_WINDOW_FREERATIO | CV_GUI_EXPANDED);
+  createTrackbar( "Amount", "Toolbar", &state.amount ,1000, on_trackbar);
+  createTrackbar( "Sigma", "Toolbar", &state.sigma ,1000, on_trackbar);
+  createTrackbar( "BG Threshold", "Toolbar", &state.threshold ,1000, on_trackbar);
   repaint();
-  waitKey(0);
+  int pressedKey = 0;
+  while(pressedKey != 27 /*Esc*/ && pressedKey != 113 /*q*/){
+    pressedKey = waitKey(0);
+    state.writeLDrawFile = pressedKey == 51 /* 3 */;
+    state.palette = Palette(argv[1]);
+    if(pressedKey==109)
+      state.showMosaic = !state.showMosaic;
+    repaint();
+    state.writeLDrawFile = false;
+  }
   if(argc >=4)
     imwrite(argv[3], state.image);
   return 0;
